398d37c492b445fee3385352449dd08e3c3d60bf
Timothée Fermeaux <fermeaux.t@gmail.com>
2021-02-11 19:18:33 +0100

    build: first deployment

---
+++ cv/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex my-4 mx-8 items-center justify-between">
+    <img src="profil.png" class="w-16 rounded-full hidden sm:block">
+    <div>
+      <NuxtLink v-for="link of links" :key="link.name" class="rounded-full p-4 pointer hover:bg-green-500 hover:text-white" :to="link.path">
+        {{ link.name }}
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      links: [
+        {
+          name: 'Home',
+          path: '/'
+        },
+        {
+          name: 'Portfolio',
+          path: '/portfolio'
+        },
+        {
+          name: 'Contact',
+          path: '/contact'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ cv/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return { hello: 'world' }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 10rem;
+}
+</style>
